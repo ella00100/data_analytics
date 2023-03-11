93ace9796655c27cdbd318fbee54e31aed9d28ec
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-03-11 17:44:56 +0900

    execel data integral

--- usingPandas/excel_data.py
+++ usingPandas/excel_data.py
@@ -7,11 +7,9 @@ total = pd.DataFrame()
 for file_name in stations_files:
     temp = pd.read_excel(file_name, header=2)
     total = pd.concat([total, temp])
-    # os.rename 함수를 이용하여 파일이름을 바꿉니다.
+total = total.sort_values(by="지역")
+total = total.reset_index(drop = True)
 
-#total = total.sort_values(by="지역")
-#total = total.reset_index(drop = True)
-    # 리스트에 저장.
 print(total)
 
 total.to_excel("전체주유소_가격.xlsx", index=False)
